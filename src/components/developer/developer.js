9f375bf6c7af3fce07bf458a9cc4e674529d26f2
Maksim Kudrautsau <ponyborn@protonmail.com>
2019-07-29 12:49:06 +0300

    feat: add developers list

---
+++ src/components/developer/developer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './developer.module.css';
+
+export default ({ name, githubName, photoURL }) => (
+  <div className={styles.developerCard}>
+    <img src={photoURL} alt={`${name}`} className={styles.developerPicture} />
+    <span>{name}</span>
+    <a href={`https://github.com/${githubName}`} className={styles.githubLink}>
+      {`github: @${githubName}`}
+    </a>
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Layout from '../components/layout/layout'
-import { graphql } from 'gatsby'
+import React from 'react';
+import Layout from '../components/layout/layout';
+import { graphql } from 'gatsby';
+import Developer from '../components/developer/developer';
+import styles from './index.module.css';
 
 export default ({ data }) => (
   <Layout>
     <p>{data.markdownRemark.frontmatter.enSiteDescription}</p>
+    <h2>Our Team</h2>
+    <div className={styles.developersBlock}>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <Developer
+          key={node.frontmatter.name}
+          name={node.frontmatter.name}
+          githubName={node.frontmatter.githubName}
+          photoURL={node.frontmatter.picture}
+        />
+      ))}
+    </div>
   </Layout>
-)
+);
 
 export const query = graphql`
   query {
@@ -15,5 +28,16 @@ export const query = graphql`
         enSiteDescription
       }
     }
+    allMarkdownRemark(filter: { frontmatter: { type: { eq: "developer" } } }) {
+      edges {
+        node {
+          frontmatter {
+            name
+            githubName
+            picture
+          }
+        }
+      }
+    }
   }
-`
+`;

08b063a5443f634d2cf27795457f6fb1208c6521
Marry705 <marry705chilo@gmail.com>
2019-07-30 20:42:31 +0300

    feat: created blog page and component with person's information

---
+++ src/pages/blog.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import PersonsContainer from '../components/developer/persons';
+
+const Persons = ({ data }) => (
+    <PersonsContainer persons={data.allDataJson.edges} />
+);
+
+export default Persons;
+
+export const query = graphql`
+  query {
+    allDataJson(sort: { fields: [lastName, firstName], order: ASC }) {
+      totalCount
+      edges {
+        node {
+          id
+          firstName
+          lastName
+          city
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;

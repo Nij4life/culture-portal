82496241ac0818aa28cbeabc1b0f5c8b3fc4b7d8
Maksim Kudrautsau <ponyborn@protonmail.com>
2019-07-31 11:55:55 +0300

    fix: fix photographer's pictures in list

--- src/components/listPhotographers/listPhotographers.js
+++ src/components/listPhotographers/listPhotographers.js
@@ -5,7 +5,7 @@ function CreateLiElement(props) {
   const { name, picture } = props.node.frontmatter;
   return (
     <li key={name} className={styles.photographersList}>
-      <img width="100" height="100" src={`${picture}`} alt={`${name}`} />
+      <img width="100" height="100" src={`${picture}`} alt={`${name}`} className={styles.photographerImage}/>
       {name}
     </li>
   );

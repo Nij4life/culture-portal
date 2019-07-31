da1cc22a0ff58090a5422498bd20c81e205b24a4
Maksim Kudrautsau <ponyborn@protonmail.com>
2019-07-31 09:43:25 +0300

    fix: fixed removed commits and filepathes Accidentaly removed a commit, try to fix that and also fix import filepath

--- src/components/listPhotographers/listPhotographers.js
+++ src/components/listPhotographers/listPhotographers.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import styles from './listPhotographers.module.css'
+import styles from './listPhotographers.module.css';
 
 function CreateLiElement(props) {
   const { name, picture } = props.node.frontmatter;
   return (
-    <li key={name} className={styles.li}>
+    <li key={name} className={styles.photographersList}>
       <img width="100" height="100" src={`${picture}`} alt={`${name}`} />
       {name}
     </li>
-  )
+  );
 }
 
-export default ({ list }) => (
-  <ul>{
-    list.map(el => CreateLiElement(el))
-  }</ul>
-);
+export default ({ list }) => <ul>{list.map(el => CreateLiElement(el))}</ul>;
--- src/pages/photographers-list.js
+++ src/pages/photographers-list.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
-import ListPhotographers from '../components/listPhotographer/listPhotographers';
+import ListPhotographers from '../components/listPhotographers/listPhotographers';
 import { graphql } from 'gatsby';
 
-export default ( {data} ) => (
+export default ({ data }) => (
   <Layout>
     <h2>Belarusian photographers</h2>
     <input type="search" placeholder="Search"></input>
     <div>
-      <ListPhotographers list = {data.allMarkdownRemark.edges}>
-      </ListPhotographers>
+      <ListPhotographers
+        list={data.allMarkdownRemark.edges}
+      ></ListPhotographers>
     </div>
   </Layout>
-)
+);
 
 export const query = graphql`
-query { 
-  allMarkdownRemark(filter: {frontmatter: {type: {eq: "photographer"}}}) {
-    edges {
-      node {
-        frontmatter {
-          name
-          picture
+  query {
+    allMarkdownRemark(
+      filter: { frontmatter: { type: { eq: "photographer" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            name
+            picture
+          }
         }
       }
     }
   }
-}
 `;

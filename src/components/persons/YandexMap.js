61659ec31794b9f427ec3b9481573d3dccdbf552
Marry705 <marry705chilo@gmail.com>
2019-08-03 18:03:26 +0300

    feat: changed style of map component

--- src/components/persons/YandexMap.js
+++ src/components/persons/YandexMap.js
@@ -9,9 +9,9 @@ export default ({markets}) => {
           apikey: 'c28eb37a-b0c8-44bd-b589-29629cb89fa2',
         }}
     >
-        <div id="map-basics" style={{ height: '50vh', width: '50vw', margin: '30px auto' }}>
-            <Map state={{center:[markets[0].latitude, markets[0].longitude], zoom: '5'}} width={600} height={400}>
-              {markets.map((item,i) => {
+        <div id="map-basics" style={{ height: '90vh', width: '90vw', margin: '30px auto' }}>
+            <Map state={{center:[markets[0].latitude, markets[0].longitude], zoom: '5'}} style={{ height: '90vh', width: '90vw' }}>
+              {markets.map( (item,i) => {
                 return(
                   <GeoObject key = {i}
                     geometry={{

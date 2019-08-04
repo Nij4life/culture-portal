0084aaa743eb5faae0da064203491cc9a8c4a3c3
Marry705 <marry705chilo@gmail.com>
2019-08-04 15:44:19 +0300

    feat: started translating the photographer’s page into English

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,60 +1,97 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { injectIntl } from 'gatsby-plugin-intl';
+import { injectIntl, IntlContextConsumer } from 'gatsby-plugin-intl';
 import Layout from '../components/layout/layout';
 import PersonsInf from '../components/persons/persons-inf';
 
 const Persons = ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <Layout>
-      <div>
-        <PersonsInf
-          name = {post.frontmatter.name}
-          picture = {post.frontmatter.picture}
-          birthDate = {post.frontmatter.birthDate}
-          deathDate = {post.frontmatter.deathDate}
-          video = {post.frontmatter.videoId}
-          coordinates = {post.frontmatter.en.coordinates}
-          lifeEvents = {post.frontmatter.en.lifeEvents}
-          works = {post.frontmatter.en.works}
-          pictures = {post.frontmatter.gallery}
-        />
-      </div>
-    </Layout>
-  )
+    <IntlContextConsumer>
+    {({ language: currentLocale }) => (
+      <Layout>
+        <div>
+          <PersonsInf
+            name = {post.frontmatter.name}
+            picture = {post.frontmatter.picture}
+            birthDate = {post.frontmatter.birthDate}
+            deathDate = {post.frontmatter.deathDate}
+            video = {post.frontmatter.videoId}
+            coordinates = {post.frontmatter.en.coordinates}
+            lifeEvents = {post.frontmatter.en.lifeEvents}
+            works = {post.frontmatter.en.works}
+            pictures = {post.frontmatter.gallery}
+          />
+        </div>
+      </Layout>
+    )}
+  </IntlContextConsumer>
+  );
 };
 
 export default injectIntl(Persons);
 
 export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
-      frontmatter {
-        en {
-          lifeEvents {
-            date
-            desctiption
-          }
-          works {
-            date
-            title
-          }
-          coordinates {
-            description
-            latitude
-            longitude
-          }
-        }
-        name
-        birthDate
-        deathDate
+query ($slug: String!) {
+  markdownRemark(fields: {slug: {eq: $slug}}) {
+    frontmatter {
+      birthDate
+      deathDate
+      picture
+      videoId
+      name
+      gallery {
         picture
-        videoId
-        gallery {
-          picture
+        title
+      }
+      ru {
+        coordinates {
+          description
+          latitude
+          longitude
+        }
+        lifeEvents {
+          date
+          desctiption
+        }
+        works {
+          date
           title
         }
+        name
+      }
+      en {
+        lifeEvents {
+          date
+          desctiption
+        }
+        works {
+          date
+          title
+        }
+        coordinates {
+          description
+          latitude
+          longitude
+        }
+      }
+      be {
+        coordinates {
+          description
+          latitude
+          longitude
+        }
+        lifeEvents {
+          date
+          desctiption
+        }
+        works {
+          date
+          title
+        }
+        name
       }
     }
-  }`;  
+  }
+}
+`;  
